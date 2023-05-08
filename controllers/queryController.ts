207e4f3f4b782a02035571ff4f31a454322ece55
Theunis Duminy <theunisduminy@ip-192-168-88-149.af-south-1.compute.internal>
2023-05-08 11:29:00 +0200

    Express folder setup

--- controllers/queryController.ts
+++ controllers/queryController.ts
@@ -1,34 +1,11 @@
-import 'dotenv/config';
-import serverless from 'serverless-http';
-import express from 'express';
-import bodyParser from 'body-parser';
+import catchAsync from '../utils/catchAsync';
+import { Request, Response, NextFunction } from 'express';
 import request from 'request';
-
-/*
-As a Javascript developer, provide me a very simple skeleton framework to start building an Android app that leverages off the ChatGPT API.
-
-Here is a very simple skeleton framework for building an Android app that leverages the ChatGPT API:
-
-1. Create a new Android project in Android Studio. 
-2. Add the ChatGPT SDK as a dependency in your project. 
-3. Create an instance of the ChatGPT class.
-4. Set up the ChatGPT listener to receive responses from the ChatGPT API.
-5. Create a UI for the app, including an input box for the user to type in their query and a list to display the response from the ChatGPT API.
-6. Connect the UI to the ChatGPT instance by sending the user query to the ChatGPT instance and displaying the response in the list.
-7. Implement additional features and customizations as needed.
-*/
-
-console.log('kicked off');
-const app = express();
-
-// Use the body-parser middleware to parse incoming request bodies
-app.use(bodyParser.json());
 
 // Replace the ChatGPT API URL with your own
 const CHATGPT_API_KEY = process.env.CHATGPT_API_KEY;
 
-// API request to ChatGPT
-app.post('/query', function (req, res) {
+export const createQuery = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   console.info('reached application');
   // Check if the query and sessionId parameters are present
   // if (!req.body.query || !req.body.sessionId) {
@@ -49,6 +26,7 @@ app.post('/query', function (req, res) {
       max_tokens: 100,
     }),
   };
+
   request.post('https://api.openai.com/v1/completions', options, function (err, response, body) {
     console.log('reached post');
     if (err) {
@@ -89,11 +67,3 @@ app.post('/query', function (req, res) {
     return res.status(200).send(data.response);
   });
 });
-
-// Start the server
-app.listen(3000, function () {
-  console.log('Server started on port 3000');
-});
-
-// Export serverless handler
-export const handler = serverless(app);
